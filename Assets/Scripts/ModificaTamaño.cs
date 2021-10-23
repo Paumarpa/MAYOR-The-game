a53b893572f5e46d53563d768bf6c584038522f1
JimmyPV <al394519@uji.es>
2021-10-23 16:48:57 +0200

    StatsAcabao
    Osea falta meter q puedas modificar la variable cuando eliges carta pero demomento esto yasta

--- Assets/Scripts/ModificaTamaño.cs
+++ Assets/Scripts/ModificaTamaño.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ModificaTamaño : MonoBehaviour
 {
+    public float ValorStat = 1.25f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this.transform.localScale = new Vector3(5f, 5f, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.localScale = new Vector3(1.25f, ValorStat, 1f);
     }
 }
