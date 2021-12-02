7bc72c65ad6543cf6005df9ec12286bc6b956d12
Manuel Martinez Giner <al398392@uji.es>
2021-12-02 15:26:20 +0100

    fix deslizar

--- Assets/Scripts/Deslizar.cs
+++ Assets/Scripts/Deslizar.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Deslizar : MonoBehaviour
+public class DeslizarP : MonoBehaviour
 {
 
-    //public static int electricidad;
-
     public GameObject carta;
-
-
-
     public GameObject baraja;
-
     public cardscript cs;
-    public ModificaTamaño stats;
+
+    public ModificaTamaño statElectricidad;
+    public ModificaTamaño statGente;
+    public ModificaTamaño statFelicidad;
+    public ModificaTamaño statDinero;
+    public ModificaTamaño statComida;
 
     SpriteRenderer spr;
     public float velCarta = .5f;
 
-    //giro carta
+
     [SerializeField]
 
     private Sprite faceSprite, backSprite, barajaSprite;
+
+
 
     private bool coroutineAllowed, facedUp;
 
@@ -35,6 +36,7 @@ public class Deslizar : MonoBehaviour
 
     public Text textAnyos;
     */
+
     [SerializeField] private BaseCarta[] cartasQueUsamos;
 
     private void SigCarta(int cartaPos)
@@ -45,22 +47,33 @@ public class Deslizar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //INICIALIZAR VALORES DE LA ESCENA
         spr = carta.GetComponent<SpriteRenderer>();
-
-
         spr.sprite = faceSprite;
         coroutineAllowed = true;
         facedUp = true;
+        cs.descripcionCarta.enabled = false;
+        cs.fondoTexto.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cs.isMouseOver)
+        {
+            cs.descripcionCarta.enabled = true;
+            cs.fondoTexto.enabled = true;
+        }
+        else
+        {
+            cs.descripcionCarta.enabled = false;
+            cs.fondoTexto.enabled = false;
+        }
         //rotacion de la carta al moverse
-        if (coroutineAllowed)   //no está en la coroutina, por lo que no choca con ello
+        if (coroutineAllowed)
+        {  //no está en la coroutina, por lo que no choca con ello
             carta.transform.rotation = Quaternion.Euler(0f, 0f, -2 * carta.transform.position.x);
-
-
+        }
 
         if (coroutineAllowed && Input.GetMouseButtonDown(1))
         {
@@ -70,11 +83,17 @@ public class Deslizar : MonoBehaviour
 
         if (carta.transform.position.x > 2) //PARA LA DERECHA
         {
-            spr.color = Color.green;
+
+            cs.descripcionLado.enabled = true;
+            cs.descripcionLado.text = cs.cartaDatos.textoDer;
+            spr.color = Color.green; //AÑADIR EFECTO PARA VER QUE SE VA A ELEGIR A LA CARTA
+
             if (Input.GetMouseButtonUp(0))
             {
-                Debug.Log("caca");
-                int aux = cs.derecha();                //restan--;
+
+                ChangeStats(true);
+                int aux = cs.derecha();
+                //restan--;
 
                 SigCarta(aux);
                 cs.UpdateCartaUI(false);
@@ -82,12 +101,15 @@ public class Deslizar : MonoBehaviour
                 //aqui se carga siguiente carta
                 //tambien se tiene que actualizar el siguiente dorso de la baraja
 
-                spr.sprite = backSprite;
-                facedUp = false;
-                cs.imagen.enabled = false;
-                cs.nombreCarta.enabled = false;
-                cs.descripcionCarta.enabled = false;
-                cs.fondoTexto.enabled = false;
+
+                //spr.sprite = backSprite;
+
+                //cs.nombreCarta.enabled = false; facedUp = false;cs.descripcionCarta.enabled = false;cs.fondoTexto.enabled = false;cs.imagen.enabled = false;
+
+
+
+                //ocultarUIcarta();
+
                 if (coroutineAllowed)
                 {
                     StartCoroutine(RotateNewCard());
@@ -96,22 +118,27 @@ public class Deslizar : MonoBehaviour
         }
         else if (carta.transform.position.x < -2)   //PARA LA IZQUIERDA
         {
-            spr.color = Color.red;
+
+            cs.descripcionLado.enabled = true;
+            cs.descripcionLado.text = cs.cartaDatos.textoIzq;
+            spr.color = Color.red; //AÑADIR EFECTO PARA VER QUE SE VA A ELEGIR A LA CARTA
+
             if (Input.GetMouseButtonUp(0))
             {
+
+
+                ChangeStats(false);
                 int aux = cs.izquierda();
                 //restan--;
 
                 SigCarta(aux);
-                cs.UpdateCartaUI(true);//aqui se carga siguiente carta
+                cs.UpdateCartaUI(true);
 
+                //aqui se carga siguiente carta
                 //tambien se tiene que actualizar el siguiente dorso de la baraja
-                spr.sprite = backSprite;
-                facedUp = false;
-                cs.imagen.enabled = false;
-                cs.nombreCarta.enabled = false;
-                cs.descripcionCarta.enabled = false;
-                cs.fondoTexto.enabled = false;
+
+                ocultarUIcarta();
+
 
                 if (coroutineAllowed)
                 {
@@ -122,6 +149,7 @@ public class Deslizar : MonoBehaviour
         }
         else
         {
+            cs.descripcionLado.enabled = false;
             spr.color = Color.white;
         }
 
@@ -133,18 +161,19 @@ public class Deslizar : MonoBehaviour
 
             Vector2 posicion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             carta.transform.position = posicion;
-            //spr.color = Color.green;
         }
         else
         {
             carta.transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), velCarta);
-            //carta.transform.rotation = Quaternion.identity; 
         }
     }
 
     private IEnumerator RotateCard(float tiempoRotacion)
     {
+
+
         float tiempoRot = tiempoRotacion;
+
         coroutineAllowed = false;
 
         if (!facedUp)
@@ -153,34 +182,21 @@ public class Deslizar : MonoBehaviour
             for (float i = 0f; i <= 90f; i += 10f)
             {
                 carta.transform.rotation = Quaternion.Euler(0f, i, 0f);
-
-
                 yield return new WaitForSeconds(tiempoRot);
             }
             cs.imagen.enabled = true;
             cs.nombreCarta.enabled = true;
-            cs.descripcionCarta.enabled = true;
             cs.fondoTexto.enabled = true;
-
             spr.sprite = faceSprite;
             for (float i = 90f; i >= 0f; i -= 10f)
             {
                 carta.transform.rotation = Quaternion.Euler(0f, i, 0f);
                 yield return new WaitForSeconds(tiempoRot);
             }
+
         }
         if (facedUp)
         {
-            /*for (float i = 180f; i >= 0f; i -= 10f)
-            {
-                carta.transform.rotation = Quaternion.Euler(0f, i, 0f);
-                if (i == 90f)
-                {
-                    spr.sprite = backSprite;
-                    cs.imagen.enabled = false;
-                }
-                yield return new WaitForSeconds(0.01f);
-            }*/
             for (float i = 0f; i <= 90f; i += 10f)
             {
                 carta.transform.rotation = Quaternion.Euler(0f, i, 0f);
@@ -188,9 +204,7 @@ public class Deslizar : MonoBehaviour
             }
             cs.imagen.enabled = false;
             cs.nombreCarta.enabled = false;
-            cs.descripcionCarta.enabled = false;
             cs.fondoTexto.enabled = false;
-
             spr.sprite = backSprite;
 
             for (float i = 90f; i >= 0f; i -= 10f)
@@ -205,6 +219,7 @@ public class Deslizar : MonoBehaviour
         coroutineAllowed = true;
 
         facedUp = !facedUp;
+        FindObjectOfType<audioManager>().Play("flipCard");
     }
 
 
@@ -216,21 +231,40 @@ public class Deslizar : MonoBehaviour
         if (!facedUp)
         {
 
-            /*for (float i = 0f; i <= 180f; i += 10f)
-            {
-                carta.transform.rotation = Quaternion.Euler(0f, i, 0f);
-                if (i == 90f)
-                {
-                    spr.sprite = faceSprite;
-                    cs.imagen.enabled = true;
 
-                }
-                yield return new WaitForSeconds(0.02f);
-            }*/
             StartCoroutine(RotateCard(0.02f));
         }
-        //coroutineAllowed = true;
 
-        //facedUp = true;
+
+    }
+
+    private void ChangeStats(bool der)
+    {
+        if (der)
+        {
+            statElectricidad.ValorStat += cs.cartaDatos.derElect * 0.1f;
+            statGente.ValorStat += cs.cartaDatos.derGente * 0.1f;
+            statFelicidad.ValorStat += cs.cartaDatos.derFelic * 0.1f;
+            statDinero.ValorStat += cs.cartaDatos.derDinero * 0.1f;
+            statComida.ValorStat += cs.cartaDatos.derComida * 0.1f;
+        }
+        else
+        {
+            statElectricidad.ValorStat += cs.cartaDatos.izqElect * 0.1f;
+            statGente.ValorStat += cs.cartaDatos.izqGente * 0.1f;
+            statFelicidad.ValorStat += cs.cartaDatos.izqFelic * 0.1f;
+            statDinero.ValorStat += cs.cartaDatos.izqDinero * 0.1f;
+            statComida.ValorStat += cs.cartaDatos.izqComida * 0.1f;
+        }
+
+    }
+
+    private void ocultarUIcarta()
+    {
+        spr.sprite = backSprite;
+        facedUp = false;
+        cs.imagen.enabled = false;
+        cs.nombreCarta.enabled = false;
+        cs.fondoTexto.enabled = false;
     }
 }
